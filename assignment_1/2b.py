1198e5c898d7b39b1eba9d4e75bd4d3ddca4a24b
Magnus Bjerke Vik <mbvett@gmail.com>
2015-10-06 13:25:05 +0200

    Add assignment 1 2b

---
+++ assignment_1/2b.py
@@ -0,0 +1,34 @@
+# Written by Magnus Bjerke Vik for IMT4202 at HiG (GUC)
+
+import math
+import matplotlib.pyplot as plt
+
+x1 = [1, 1, 1, 1]
+x2 = [1, 0, 1, 0, 1, 0, 1, 0]
+x3 = [1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0]
+
+sequences = [x1, x2, x3]
+colors = ['green', 'red', 'blue']
+linestyles = ['-', '--', ':']
+
+for i, sequence in enumerate(sequences):
+    num_samples = len(sequence)
+
+    magnitudes = []
+    neg_two_pi = -2.0 * math.pi
+
+    for k in range(10):
+        dft = 0.0
+        neg_two_pi_k = neg_two_pi * float(k)
+
+        for m, sample in enumerate(sequence):
+            param = neg_two_pi_k * float(m) / float(num_samples)
+            dft = dft + sample * (math.cos(param) + 1.0j * math.sin(param))
+
+        magnitude = abs(dft)
+        magnitudes.append(magnitude)
+
+    plt.plot(magnitudes, color=colors[i], linestyle=linestyles[i], linewidth=float(len(sequences) - i) * 3.0)
+
+plt.show()
+
