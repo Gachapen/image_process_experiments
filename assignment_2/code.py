ba38a36b9c37e82f3f1dec842499df7135936cd1
Magnus Bjerke Vik <mbvett@gmail.com>
2015-10-27 23:21:59 +0100

    Merge master

--- assignment_2/code.py
+++ assignment_2/code.py
@@ -1,6 +1,6 @@
 from PIL import Image
 import numpy as np
-import scipy.fftpack as fp 
+import scipy.fftpack as fp
 
 def blockproc(im, fun):
     blocksize = (8, 8)
@@ -14,7 +14,7 @@ def blockproc(im, fun):
             yb_proc = fun(yb)
             yblocks_proc.append(yb_proc)
         xblocks_proc.append(np.concatenate(yblocks_proc, axis=1))
-    
+
     proc = np.concatenate(xblocks_proc, axis=0)
 
     return proc
@@ -25,18 +25,27 @@ def dct2(im):
 def idct2(im):
     return fp.idct(fp.idct(im, norm='ortho', axis=0), norm='ortho', axis=1)
 
+def compress(im):
+    dct = fp.dct(fp.dct(im, norm='ortho', axis=0), norm='ortho', axis=1)
+    return dct
+
 image = Image.open('lena.png').convert('L')
 im = np.array(image, dtype=np.float)
 im_dct = blockproc(im, dct2)
 im_dft = blockproc(im, fp.fft2)
+im_compress = blockproc(im, compress)
 
 #imagedct = Image.fromarray(imdct)
 #imagedct.show()
 
 im_idct = blockproc(im_dct, idct2)
-image_idct = Image.fromarray(im_idct)
-image_idct.show();
+image_idct = image.fromarray(im_idct)
+image_idct.show()
 
-im_idft = blockproc(im_dft, fp.ifft2)
-image_idft = Image.fromarray(im_idft)
-image_idft.show()
+# im_idft = blockproc(im_dft, fp.ifft2)
+# image_idft = Image.fromarray(im_idft)
+# image_idft.show()
+
+im_uncompress = blockproc(im_compress, idct2)
+image_uncompress = image.fromarray(im_uncompress)
+image_uncompress.show()
